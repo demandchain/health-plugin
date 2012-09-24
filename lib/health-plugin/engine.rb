fe416cdd9e76622c20459ca67305a3b625368580
Zachary Patten <zim931@gmail.com>
2012-09-24 15:07:22 -0700

    place the health-plugin as the first entry in the middleware stack

--- lib/health-plugin/engine.rb
+++ lib/health-plugin/engine.rb
@@ -2,7 +2,8 @@ module HealthPlugin
 
   class Engine < Rails::Engine
     isolate_namespace HealthPlugin
-    config.app_middleware.use HealthPlugin::Rack::Monitor
+
+    config.app_middleware.insert(0, HealthPlugin::Rack::Monitor)
   end
 
 end
